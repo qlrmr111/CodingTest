4508eaa4614c85efed51167185ae01ef04d0ff3e
burst-cannon <qlrmr111@naver.com>
2023-10-17 21:56:22 +0900

    add README,1,2

---
+++ CodingTest_1_array/CodingTest_1_array/main.swift
@@ -0,0 +1,19 @@
+//
+//  main.swift
+//  CodingTest_1_array
+//
+//  Created by 권운기 on 10/17/23.
+//
+
+import Foundation
+
+func solution(_ num_list:[Int]) -> [Int] {
+    var array = num_list
+    guard array.count < 1000 else {return [0]}
+     
+    for i in 0...num_list.count-1 {
+        guard array[i] < 1000 else {return [0]}
+        array[i] = num_list[num_list.count-1-i]
+    }
+    return array
+}
---
+++ CodingTest_2_Number/CodingTest_2_Number/main.swift
@@ -0,0 +1,21 @@
+//
+//  main.swift
+//  CodingTest_2_Number
+//
+//  Created by 권운기 on 10/17/23.
+//
+
+import Foundation
+
+func solution(_ n:Int) -> Int {
+    guard n<=1000000, n>0 else {return 0}
+    var num = 0
+    for i in 1...n {
+        for j in 1...(n/i){
+            if i * j == n {
+                num+=1
+            }
+        }
+    }
+    return num
+}
